f2f1da55d71f567aaf76a2e0443b8edd2a491db7
Denis Hodymchuk <homyk.den2609@gmail.com>
2026-05-06 00:23:12 +0300

    Fix: Resolve hydration mismatch by moving scripts in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,11 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="uk" className="h-full antialiased" suppressHydrationWarning>
-      <head>
+      <body className={`${geist.className} min-h-full flex flex-col`} suppressHydrationWarning>
         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
         <Script src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js" strategy="afterInteractive" />
-      </head>
-      <body className={`${geist.className} min-h-full flex flex-col`} suppressHydrationWarning>
         {children}
       </body>
     </html>
