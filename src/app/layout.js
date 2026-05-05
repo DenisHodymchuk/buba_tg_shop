f14da92f9f4bcdf6a0c03d04e4642c50e16eb6ef
Denis Hodymchuk <homyk.den2609@gmail.com>
2026-05-06 00:24:03 +0300

    Fix: Revert script strategy to afterInteractive to satisfy React

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,11 +11,9 @@ export const metadata = {
   description: "Преміальні 3D іграшки в Telegram",
 };
 
-export default function RootLayout({ children }) {
-  return (
     <html lang="uk" className="h-full antialiased" suppressHydrationWarning>
       <body className={`${geist.className} min-h-full flex flex-col`} suppressHydrationWarning>
-        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="afterInteractive" />
         <Script src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js" strategy="afterInteractive" />
         {children}
       </body>
